33a4c6978103e3c9d33f3331b730f1f4ff2b8783
nandkishor-agnext <nandkishore@agnext.in>
2021-11-17 12:26:39 +0530

    formatting

--- models/commodity-category.js
+++ models/commodity-category.js
@@ -3,8 +3,12 @@ const mongoose= require('mongoose');
 
 
 const CommodityCategory = mongoose.model('CommodityCategory',new mongoose.Schema({
-    name:{type:String,required:true},
-    isActive:{type:Boolean,default:true}
+    name:{
+        type:String,
+        required:true},
+    isActive:{
+        type:Boolean,
+        default:true}
 }))
 
 exports.CommodityCategory = CommodityCategory;
--- models/commodity.js
+++ models/commodity.js
@@ -3,10 +3,18 @@ const mongoose= require('mongoose');
 
 
 const Commodity = mongoose.model('Commodity',new mongoose.Schema({
-    categoryId:{type:String,required:true},
-    categoryName:{type:String,required:true},
-    name:{type:String,required:true},
-    isActive:{type:Boolean,default:true}
+    categoryId:{
+        type:String,
+        required:true},
+    categoryName:{
+        type:String,
+        required:true},
+    name:{
+        type:String,
+        required:true},
+    isActive:{
+        type:Boolean,
+        default:true}
 }))
 
 exports.Commodity = Commodity;
